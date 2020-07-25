dc63932934b9fbae8b4da43d3d2affc85d276109
BorisovecViktor <viktorborisovets@gmail.com>
2020-07-26 00:08:45 +0300

    Updates

---
+++ precache-manifest.0b07af34d4402b649be72869d4210d54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfaf6f346194e1dea3937927e429527",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc27c36c7e7bbbc3ef4f",
+    "url": "./static/css/main.2cbac8cb.chunk.css"
+  },
+  {
+    "revision": "e7986e08efaf63327f12",
+    "url": "./static/js/2.7882e4b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.7882e4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc27c36c7e7bbbc3ef4f",
+    "url": "./static/js/main.554f6ce9.chunk.js"
+  },
+  {
+    "revision": "1b3cfc1dab9e8eaaed46",
+    "url": "./static/js/runtime-main.fe0be788.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "./static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "6f553d93f2b3773340b890adab9ca954",
+    "url": "./static/media/close.6f553d93.svg"
+  }
+]);
